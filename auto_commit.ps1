396062ce107e311c7ac1cf1bf94f8d1deaa65bee
Rushi05112 <rushikesh.jare@woodside.com>
2026-03-01 22:32:07 +0530

    Auto commit at 20260301_223207

---
+++ auto_commit.ps1
@@ -0,0 +1,19 @@
+# Create timestamp
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+
+# File name
+$filename = 'auto_$timestamp.txt'
+
+# Create file with content
+"This is auto commit at $timestamp" | Out-File $filename
+
+#Git add
+git add .
+
+# git commit
+git commit -m "Auto commit at $timestamp"
+
+# Git Push
+git push origin main
+
+Write-Host "Auto commit completed."
